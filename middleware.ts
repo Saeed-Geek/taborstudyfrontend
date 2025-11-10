1f33b70e9e7849aff254052f7c42b5f31e9137db
Saeed-Geek <feautredgaming@gmail.com>
2025-11-10 15:25:25 +0200

    frontend changes

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,6 @@ import type { NextRequest } from 'next/server';
 const PUBLIC_PATHS = [
   '/login',
   '/register',
-  '/greet',
   '/reset-password',
   '/check-email',
   '/otp-verify',
@@ -13,25 +12,25 @@ const PUBLIC_PATHS = [
 ];
 
 export function middleware(request: NextRequest) {
-//   const { pathname } = request.nextUrl;
+  const { pathname } = request.nextUrl;
 
-//   // Get the token — from cookies (or headers)
-//   const accessToken = request.cookies.get('accessToken')?.value
+  // Get the token — from cookies (or headers)
+  const accessToken = request.cookies.get('accessToken')?.value
 
-//   // Check if the route is in the PUBLIC_PATHS list
-//   const isPublicRoute = PUBLIC_PATHS.some((path) => pathname.startsWith(path));
-// console.log(accessToken,isPublicRoute)
-//   // 🔒 1. If user is NOT logged in and tries to access a protected route
-//   if (!isPublicRoute && !accessToken) {
-//     return NextResponse.redirect(new URL('/login', request.url));
-//   }
+  // Check if the route is in the PUBLIC_PATHS list
+  const isPublicRoute = PUBLIC_PATHS.some((path) => pathname.startsWith(path));
+console.log(accessToken,isPublicRoute)
+  // 🔒 1. If user is NOT logged in and tries to access a protected route
+  if (!isPublicRoute && !accessToken) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
 
-//   // 🚫 2. If user IS logged in and tries to access a public route (like /login)
-//   if (isPublicRoute && accessToken) {
-//     return NextResponse.redirect(new URL('/greet', request.url));
-//   }
+  // 🚫 2. If user IS logged in and tries to access a public route (like /login)
+  if (isPublicRoute && accessToken) {
+    return NextResponse.redirect(new URL('/greet', request.url));
+  }
 
-  // ✅ 3. Otherwise, allow the request to go through
+  ✅ 3. Otherwise, allow the request to go through
   return NextResponse.next();
 }
 
